c4a446fe32edb1c2228982499e6d8fb8e881ef61
Subhan Ali <subhanali.softengg@gmail.com>
2024-06-01 15:15:53 +0400

    Slight issue fixed.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  
+  // Update the query to match the text more flexibly
   const linkElement = screen.getByText(/learn react/i);
+  
+  // Assert that the element is in the document
   expect(linkElement).toBeInTheDocument();
 });
